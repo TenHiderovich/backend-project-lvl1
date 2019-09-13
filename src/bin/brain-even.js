6ad0e561cb60fbaa8a04970c182ea6af805a1da3
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2019-09-13 08:58:18 +0800

    Добавил приветствие в brain-even

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,3 +1,6 @@
 import startGame from '..';
 
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
 startGame();
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -2,6 +2,5 @@ import setName from '..';
 
 
 console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if the number is even, otherwise answer "no"');
 
 setName();
