455a8fd260767ebf9534701457723c9fe943ac4c
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2019-11-22 15:44:36 +0800

    Скорректировал названия переменых

--- src/games/calc.js
+++ src/games/calc.js
@@ -2,31 +2,25 @@ import gameEngine from '..';
 
 import getRandomValue from '../getRandomValue';
 
-const addNumbers = (number1, number2) => number1 + number2;
-const subtractNumbers = (number1, number2) => number1 - number2;
-const multiplyNumbers = (number1, number2) => number1 * number2;
-
 const introductoryQuestion = 'What is the result of the expression?';
 
-const mathematicalExpressions = {
-  '+': addNumbers,
-  '-': subtractNumbers,
-  '*': multiplyNumbers,
+const operations = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
 };
 
 const getGameData = () => {
   const firstValue = getRandomValue(1, 100);
   const secondValue = getRandomValue(1, 100);
 
-  const mathematicalExpressionsKeys = Object.keys(mathematicalExpressions);
-  const randomValue = getRandomValue(0, mathematicalExpressionsKeys.length - 1);
-  const mathematicalExpressionKey = mathematicalExpressionsKeys[randomValue];
+  const operationsKeys = Object.keys(operations);
+  const indexOperation = getRandomValue(0, operationsKeys.length - 1);
+  const operationKey = operationsKeys[indexOperation];
+  const selectedOperation = operations[operationKey];
 
-  const selectedMathematicalExpression = mathematicalExpressions[mathematicalExpressionKey];
-  const resultExpression = selectedMathematicalExpression(firstValue, secondValue);
-
-  const correctAnswer = String(resultExpression);
-  const question = `${firstValue} ${mathematicalExpressionKey} ${secondValue}`;
+  const correctAnswer = String(selectedOperation(firstValue, secondValue));
+  const question = `${firstValue} ${operationKey} ${secondValue}`;
 
   return {
     correctAnswer,
